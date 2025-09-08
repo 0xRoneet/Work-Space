6318d2032c904f5366a4efe7b1d65323cff6b29f
Yadav <2441335@cognizant.com>
2025-09-08 13:59:48 +0530

    Day-04

---
+++ Day-04/src/Practice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <div>
+        <h1>List Of Mobile Phones</h1>
+      <ul>
+        <li>iPhone 14</li>
+        <li>Samsung Galaxy S22</li>
+        <li>Google Pixel 6</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Practice
